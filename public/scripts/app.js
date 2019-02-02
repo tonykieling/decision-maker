ccd34a6b6ad6211d629b27ac1fbb262b76d02a71
samanthamarienock <sammarienock@gmail.com>
2019-02-01 16:14:15 -0800

    makes sortable polling page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,5 +8,15 @@ $(() => {
       // $("<div>").text(user.name).appendTo($("body"));
       $("<div>").text(`${user.email} - ${user.password}`).appendTo($("body"));
     }
-  });;
+  });
+  $(function() {
+    $('.vote_sort').sortable();
+  });
+  $('.vote_submit').on('click', function(event) {
+    event.preventDefault();
+    let voteArray = [];
+    $(li).each(function(index, li) {
+      voteArray.push($li)
+    });
+  });
 });
