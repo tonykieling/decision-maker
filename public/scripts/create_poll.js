c85b8f3984b6b1a84f79f5f5a1aadae4f29483ef
Tony Kieling <tony.kieling@gmail.com>
2019-02-02 16:33:22 +0000

    adding dynamically starting work

--- public/scripts/create_poll.js
+++ public/scripts/create_poll.js
@@ -1,43 +1,34 @@
 
 $(function() {
-  let count = 0;
+  // let count = 0;
 
-  $("#send_options").on('click', function(event) {
-    count += 1;
+  // $(".add_options").on('click', function(event) {
 
-    event.preventDefault();
-    console.log("create poll button");
-
-    const data = $("#option").val();
-    console.log("data: ", data);
-
-    // $("#option").clone(true).val("").appendTo(".options");
-
-    let newItem = $("<div>");
-
-    $(newItem).val(data);
-    console.log("newItem: ", newItem);
-    $(".options").prepend($(newItem));
-    // $(".options").text($("#option").val());
-    console.log($(".options"));
-    $("#option").val("");
-
+  $('.add').click(function() {
+      $('.block:last').before('<div class="block"><input type="text" /><span class="remove">Remove Option</span></div>');
   });
+  $('.optionBox').on('click','.remove',function() {
+     $(this).parent().remove();
+  });
+
+    // count += 1;
+
+    // event.preventDefault();
+    // console.log("create poll button");
+
+    // const data = $("#option").val();
+    // console.log("data: ", data);
+
+    // // $("#option").clone(true).val("").appendTo(".options");
+
+    // let newItem = $("<div>");
+
+    // $(newItem).val(data);
+    // console.log("newItem: ", newItem);
+    // $(".options").prepend($(newItem));
+    // // $(".options").text($("#option").val());
+    // console.log($(".options"));
+    // $("#option").val("");
+
 });
 
-
-// $("#add-item").click(function() {
-//   var text = $("#text").val();
-//   jQuery.post('http://127.0.0.1:5000/api/v2/item', { text: text } , function(data) {
-//       console.log(data);
-//       if (data["error"]) {
-//           $("#msg").html('Error: ' + data["error"]);
-//       }
-//       if (data["ok"]) {
-//           $("#msg").html('Item ' + data["text"] + ' added');
-//       }
-//       show_items();
-
-//   });
-//  return false;
-// });
