841a26a4575386b8dd03230503f9a907a97ffa85
samanthamarienock <sammarienock@gmail.com>
2019-02-01 16:31:55 -0800

    updates bug

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,17 +1,18 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      console.log("#### app.js");
-      // $("<div>").text(user.name).appendTo($("body"));
-      $("<div>").text(`${user.email} - ${user.password}`).appendTo($("body"));
-    }
-  });
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((users) => {
+  //   for(user of users) {
+  //     console.log("#### app.js");
+  //     // $("<div>").text(user.name).appendTo($("body"));
+  //     $("<div>").text(`${user.email} - ${user.password}`).appendTo($("body"));
+  //   }s
+  // });
   $(function() {
     $('.vote_sort').sortable();
-  });
+  })
+
   $('.vote_submit').on('click', function(event) {
     event.preventDefault();
     let voteArray = [];
