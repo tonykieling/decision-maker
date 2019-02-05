637712bbd12af3850f9385e9e4bdb121b109673f
Tony Kieling <tony.kieling@gmail.com>
2019-02-05 02:01:25 +0000

    doing cleanup

--- db/migrations/20190131194433_create_option.js
+++ db/migrations/20190131194433_create_option.js
@@ -4,7 +4,6 @@ exports.up = function(knex, Promise) {
     knex.schema.createTable('option', function(table){
       table.increments('id');
       table.string('label');
-      table.integer('score');
       table.integer('poll_id').unsigned().notNullable();
       table.foreign('poll_id').references('id').inTable('poll');
     })
